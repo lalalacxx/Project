17509c4dd20a92538e20b031c32afb0ca48d590a
lalalacxx <2420059729@qq.com>
2018-07-20 08:28:15 -0700

    def.h文件

---
+++ tetris/Tetris/include/def.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define FC 3
+#define BC 7
+
+struct shape
+{
+    int s[5][5];
+};
+struct data
+{
+    int x;
+    int y;
+};
+extern struct data t;
+extern struct shape shape_arr[7];
