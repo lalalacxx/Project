5ae4b1abdbb44b60a9947d66dc601566fb2c314b
lalalacxx <2420059729@qq.com>
2018-07-24 06:44:53 -0700

    各种功能函数的实现

---
+++ Tetris/src/tetris.c
@@ -0,0 +1,214 @@
+#include <stdio.h>
+#include <def.h>
+#include <string.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <keyboard.h>
+#include <tetris.h>
+#include <stdlib.h>i
+
+//游戏结束
+int quit()
+{
+    int i = 0;
+    for(i = 0;i < W;i++)
+    {
+        if(background[1][i] != 0)
+        {
+            return 1; 
+        }
+    }
+    return 0;
+}
+void draw_element(int x, int y, int c)
+{
+	x *= 2;
+	x++;
+	y++;
+
+	printf("\033[%d;%dH\033[?25l", y, x);
+	printf("\033[3%dm\033[4%dm[]", c, c);
+	fflush(stdout);
+	printf("\033[0m");
+}
+//展示下一个将要出现的图形
+void show_next( void )
+{
+    int i = 0;
+	for (i=0; i<5; i++) {
+        int j = 0;
+		for (j=0; j<5; j++) {
+			if ( shape_arr[next].s[i][j] == 0 ) {
+				draw_element(W+2+j, 2+i, BC);
+			} else {
+				draw_element(W+2+j, 2+i, FC);
+			}
+		}
+	}
+}
+//消除被填满的行
+void clean_line( void )
+{
+    int i = 0;
+	for (i=0; i<H; i++) {
+		int t = 0;
+        int j = 0;
+		for (j=0; j<W; j++){
+			if( background[i][j] != 0 )
+				t++;
+		}
+		if ( t == W ) {
+            int k = 0;
+			for (k=i; k>0; k--) {
+				memcpy(background[k], background[k-1], 
+								sizeof background[i]);
+			}
+			memset(background[0], 0x00, sizeof background[0]);
+		}
+	}
+}
+
+void draw_shape(int x, int y, struct shape p, int c)
+{
+    int i = 0;
+	for (i=0; i<5; i++) {
+        int j = 0;
+		for (j=0; j<5; j++) {
+			if ( p.s[i][j] != 0 )
+				draw_element(x+j, y+i, c);
+		}
+	}
+}
+
+int can_move(int x, int y, struct shape sp)
+{
+    int i = 0;
+	for (i=0; i<5; i++) {
+        int j = 0;
+		for (j=0; j<5; j++) {
+			if ( sp.s[i][j] == 0 )
+				continue;
+			if ( x+j < 0 )
+				return 0;
+			if ( x+j >= W )
+				return 0;
+			if ( y+i >= H )
+				return 0;
+			if ( background[y+i][x+j] != 0 )
+				return 0;
+		}
+	}
+
+	return 1;
+}
+//绘制游戏背景
+void draw_back( void )
+{
+    int i= 0;
+	for (i=0; i<H; i++) {
+        int j = 0;
+		for (j=0; j<W; j++) {
+			if ( background[i][j] != 0 ) {
+				draw_element(j, i, FC);
+			} else {
+				draw_element(j, i, BC);
+			}
+		}
+	}
+}
+//设置游戏背景相关属性
+void set_back(int x, int y, struct shape sh)
+{
+    int i = 0;
+	for (i=0; i<5; i++) {
+        int j = 0;
+		for (j=0; j<5; j++) {
+			if (sh.s[i][j] != 0 ) {
+				background[y+i][x+j] = FC;
+			}
+		}
+	}
+}
+
+void timer_tetris(struct data *p)
+{
+	draw_shape(p->x, p->y, shape_arr[cur], BC);
+	if ( can_move(p->x, p->y+1, shape_arr[cur]) ) {
+	    draw_shape(p->x, p->y, shape_arr[cur], BC);
+		p->y++;
+	} else {
+		set_back(p->x, p->y, shape_arr[cur]);
+		clean_line();
+        if(quit() == 1)
+        {
+            recover_keyboard();
+            printf("\033[?25h");
+            exit(0);
+        }
+		draw_back();
+		t.x = 1;
+		t.y = 0;
+		cur = next;
+		next = rand() % 7;
+		show_next();
+	}
+	draw_shape(p->x, p->y, shape_arr[cur], FC);
+}
+//图形的旋转
+struct shape turn_90(struct shape sp)
+{
+	struct shape t;
+	int i = 0;
+	for (i=0; i<5; i++){
+        int j = 0;
+		for (j=0; j<5; j++){
+			t.s[i][j] = sp.s[4-j][i];
+		}
+	}
+
+	return t;
+}
+//键的控制
+int tetris(struct data *p)
+{
+	int ret = 0;
+	int c = get_key();
+	if ( is_up(c) ) {
+		draw_shape(p->x, p->y, shape_arr[cur], BC);
+		shape_arr[cur] = turn_90(shape_arr[cur]);
+		if ( !can_move(p->x, p->y, shape_arr[cur]) ) {
+			shape_arr[cur] = turn_90(shape_arr[cur]);
+			shape_arr[cur] = turn_90(shape_arr[cur]);
+			shape_arr[cur] = turn_90(shape_arr[cur]);
+		}
+		draw_shape(p->x, p->y, shape_arr[cur], FC);
+	}
+	if ( is_down(c) ) {
+		draw_shape(p->x, p->y, shape_arr[cur], BC);
+		if ( can_move(p->x, p->y+1, shape_arr[cur]) ) {
+			p->y++;
+		}
+		draw_shape(p->x, p->y, shape_arr[cur], FC);
+	}
+	if ( is_left(c) ) {
+		draw_shape(p->x, p->y, shape_arr[cur], BC);
+		if ( can_move(p->x-1, p->y, shape_arr[cur]) ) {
+			p->x--;
+		}
+		draw_shape(p->x, p->y, shape_arr[cur], FC);
+	}
+	if ( is_right(c) ) {
+		draw_shape(p->x, p->y, shape_arr[cur], BC);
+		if ( can_move(p->x+1, p->y, shape_arr[cur]) ) {
+			p->x++;
+		}
+		draw_shape(p->x, p->y, shape_arr[cur], FC);
+	}
+	if ( is_esc(c) ) {
+		ret = 1;
+	}
+
+	return ret;
+}
+
